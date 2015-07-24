070c05fa32cdd6fdc69d92f08730f0577cf0bfc5
DanielAlk <daniodfad@yahoo.com.ar>
2015-07-23 23:39:22 -0300

    Fix execute buffer on not found, only execute when method exists

--- public/index.php
+++ public/index.php
@@ -2,5 +2,5 @@
 include '../environment/main.php';
 if (isset($page_headers)) foreach ($page_headers as $header) header($header);
 include $app->layout;
-$app->buffer();
+if (method_exists($app, 'buffer')) $app->buffer();
 ?>
