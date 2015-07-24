c3dce74a665d393ada27b0f04a9ebf6936b4c363
DanielAlk <daniodfad@yahoo.com.ar>
2015-07-24 00:41:49 -0300

    Saving methods on executions, instead of decifring it later

--- environment/path.php
+++ environment/path.php
@@ -2,6 +2,7 @@
 class Path {
 
 	private $routes;
+	private $paths;
 
 	# TODO: make this function recognize more than one pattern defined in routes.ini
 	public function __construct() {
@@ -39,76 +40,24 @@ class Path {
 				}
 			}
 		}
+		$this->create_path_methods();
 		$GLOBALS['params'] = $params;
 	}
 
-	private function params_from_key($key, $params) {
+	private function params_from_key($key, $params = array()) {
 		if(strpos($key, '#') !== false) list($controller, $action) = explode('#', $key);
-		else {
-			$controller = $key;
-			$action = 'index';
-		};
+		else { $controller = $key; $action = 'index'; };
 		$params['controller'] = $controller;
 		$params['action'] = $action;
 		return $params;
 	}
 
-	public function __call($name, $args) {
-		$absolute_path = $name[0] == '_';
-		if ($absolute_path) $name = substr($name, 1);
-		if (strpos($name, '_')) $name = str_replace('__', '#', $name);
-		if (isset($args) && isset($args[0])) {
-			if (is_object($args[0]) || is_array($args[0])) $arr = (array)$args[0];
-			else if (is_string($args[0])) $arr = $args[0];
+	private function create_path_methods() {
+		foreach ($this->routes as $key => $value) {
+			$params = $this->params_from_key($key);
+			if (strpos($key, '#') !== false) $key = str_replace('#', '_', $key);
+			$this->paths[$key] = array('controller' => $params['controller'], 'action' => $params['action'], 'route' => $value);
 		}
-		
-		if (isset($this->routes[$name])) {
-			$route = $this->routes[$name];
-			if (isset($arr) && is_array($arr)) {
-				//SI EN LA URL DEFINIDA FIGURA ALGO TIPO: {id} , ESO SERA REEMPLAZADO POR $arr['id'];
-				if (preg_match_all('/{\w+}/', $route, $results)) {
-					$prop_names = $props = array();
-					foreach($results as $result) foreach(preg_replace('/[{}]/', '', $result) as $p) $prop_names[] = $p;
-					foreach ($prop_names as $key => $prop_name) if (isset($arr[$prop_name])) {
-						$prop = $arr[$prop_name];
-						unset($arr[$prop_name]);
-						$props[] = $prop;
-					}
-					if (count($props)) {
-						$route = str_replace($prop_names, $props, $route);
-						$route = str_replace(array('{','}'), '', $route);
-					}
-					//SI PROPIEDADES DEL ARRAY NO FUERON REEMPLAZADAS SE CONVIERTEN A QUERY
-					if (count($arr)) {
-						$route.='?'.http_build_query($arr);
-					}
-				}
-				//SI EN LA URL NO HAY NADA QUE REEMPLAZAR EL ARRAY SE CONVIERTE A QUERY
-				else $route.='?'.http_build_query($arr);
-			}
-		}
-		else if(strpos($name, '#')) {
-			list($controller, $action) = explode('#', $name);
-			$route = $controller.'/'.$action.'/';
-		}
-		else {
-			$route = $name.'/';
-		}
-
-		if (isset($arr)) {
-			if (!isset($this->routes[$name]) && is_array($arr)) $route.='?'.http_build_query($arr);
-			else if (is_string($arr)) $route.=$arr;
-		}
-
-		//CLEAN ROUTE IF PATTERN STILL THERE
-		$route = preg_replace('/{\w+}\//', '', $route);
-		//CLEAN ROUTE IF DOUBLE BAR FOUND
-		$route = preg_replace('/\/\//', '/', $route);
-
-		global $base_url, $host;
-
-		if ($absolute_path) return $host.$base_url.$route;
-		else return $base_url.$route;
 	}
 
 }
