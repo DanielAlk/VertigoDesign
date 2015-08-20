19e7103d62d5e0f9df32590ca27323a9411c9785
DanielAlk <daniodfad@yahoo.com.ar>
2015-08-20 08:54:53 -0300

    google site verification

--- app/views/layouts/_head.php
+++ app/views/layouts/_head.php
@@ -3,6 +3,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <meta name="description" content="">
 <meta name="author" content="">
-<meta name="google-site-verification" content="6IM8lSoVCSNyAMjuWi_51GEKeL4tAn6BZF9F-5iuFhE" />
+<meta name="google-site-verification" content="m0GDkk5BzrixiSGHQKpr2omEIA_brrhZdIdVt05Jqfg" />
 <title><?php l('main.title'); ?></title>
 <link href="<?php $asset->path('favicon.ico'); ?>" rel="icon" type="image/x-icon">
